e27a49dae6ffd4041d9c73e5547d65fe4aa5354f
deveHk <connect.to.hk@gmail.com>
2023-03-17 16:30:49 +0530

    feature: events page

--- components/SideNavbar/SideMenu.tsx
+++ components/SideNavbar/SideMenu.tsx
@@ -1,14 +1,44 @@
 import Image from "next/image";
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { GiAbstract092, GiComputerFan, GiDividedSquare } from "react-icons/gi";
+
+import { useRouter } from "next/router";
 import useScrollAnimation from "../Hooks/useScrollAnimation";
 
 const SideMenu = () => {
+  const hasWindow = typeof window !== "undefined";
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return {
+      width,
+      height,
+    };
+  }
+  /*  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );*/
+
+  function handleResize() {
+    let ele = getWindowDimensions();
+    if (ele.width && ele.width >= 1080) {
+      setToggle(true);
+    } else {
+      setToggle(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   const ani = useScrollAnimation(
     ["opacity-100", "translate-x-[0px]", "rotate-0"],
     ["opacity-0", "translate-x-[10px]", "rotate-45"]
   );
   const [toggle, setToggle] = useState(false);
+  const router = useRouter();
   return (
     <div className="fixed left-0 top-0 z-10">
       <div
@@ -51,33 +81,59 @@ const SideMenu = () => {
         <div className="h-full backdrop-blur-[10px] relative">
           <div
             ref={ani}
-            className="opacity-0 rotate-45 origin-top translate-x-[10px] transition-[opacity,_transform] duration-700 w-full  h-full flex-col flex justify-center"
+            className="opacity-0 rotate-45 origin-top translate-x-[10px] transition-[opacity,_transform] duration-700 w-full  h-full flex-col flex justify-center items-center "
           >
-            <div className="flex flex-col items-center  justify-center m-10">
-              <GiDividedSquare
-                className="text-[#ECE2FF] h-[60px] w-[60px]
+            <Link href="/" className="w-full flex justify-center">
+              <div
+                className={`${
+                  router.asPath.split("/").at(1) === ""
+                    ? "bg-gradient-to-r drop-shadow-lightestbold to-[#00435B]  from-[#0CC0FF]"
+                    : ""
+                } flex flex-col items-center  justify-center w-[80%] h-[100px] rounded-2xl my-10`}
+              >
+                <GiDividedSquare
+                  className="text-[#ECE2FF] h-[60px] w-[60px]
           "
-              />
-              <div className="text-[#ECE2FF] text-lg font-semibold">HOME</div>
-            </div>
-            <div className="flex flex-col items-center justify-center mb-10">
-              <GiAbstract092
-                className="text-[#ECE2FF] h-[60px] w-[60px]
-          "
-              />
-
-              <div className="text-[#ECE2FF] text-lg font-semibold">EVENTS</div>
-            </div>
-            <div className="flex flex-col items-center  justify-center ">
-              <GiComputerFan
-                className="text-[#ECE2FF] h-[60px] w-[60px]
-          "
-              />
-
-              <div className="text-[#ECE2FF] text-lg font-semibold">
-                HACKATHON
+                />
+                <div className="text-[#ECE2FF] text-lg font-semibold">HOME</div>
               </div>
-            </div>
+            </Link>
+            <Link href="/events" className="w-full flex justify-center">
+              <div
+                className={`${
+                  router.asPath.split("/").at(1) === "events"
+                    ? "bg-gradient-to-r drop-shadow-lightestbold to-[#00435B]  from-[#0CC0FF]"
+                    : ""
+                } flex flex-col items-center  justify-center w-[80%] h-[100px] rounded-2xl my-10`}
+              >
+                <GiAbstract092
+                  className="text-[#ECE2FF] h-[60px] w-[60px]
+          "
+                />
+
+                <div className="text-[#ECE2FF] text-lg font-semibold">
+                  EVENTS
+                </div>
+              </div>
+            </Link>
+            <Link href="/hackathon" className="w-full flex justify-center">
+              <div
+                className={`${
+                  router.asPath.split("/").at(1) === "hackathon"
+                    ? "bg-gradient-to-r drop-shadow-lightestbold to-[#00435B]  from-[#0CC0FF]"
+                    : ""
+                } flex flex-col items-center  justify-center w-[80%] h-[100px] rounded-2xl my-10`}
+              >
+                <GiComputerFan
+                  className="text-[#ECE2FF] h-[60px] w-[60px]
+          "
+                />
+
+                <div className="text-[#ECE2FF] text-lg font-semibold">
+                  HACKATHON
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
