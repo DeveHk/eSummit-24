1c1f74dd3f48b20830ca90e5d3062b5a55ff8c58
deveHk <connect.to.hk@gmail.com>
2023-03-17 18:45:44 +0530

    feature: refeatured

--- components/Events/Event.tsx
+++ components/Events/Event.tsx
@@ -1,10 +1,11 @@
 import SideMenu from "../SideNavbar/SideMenu";
+import EventSection from "./EventSection";
 
 const Event = () => {
   return (
     <div className="bg-[#231F20] w-full min-h-[100vh] relative">
       <SideMenu />
-      {/*<EventSection />*/}
+      <EventSection />
     </div>
   );
 };
